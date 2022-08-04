1239bd51249c5dc8b21618f17523d89cf4aaa1e4
Muriithi Ian <imuriithiian@gmail.com>
2022-08-04 13:54:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+	if (parseInt(dodger.style.left) - 1 < 0) {
+		alert("edge of screen");
+		return;
+	} else {
+		dodger.style.left = parseInt(dodger.style.left) - 1 + "px";
+	}
+}
+function moveDodgerRight() {
+	if (parseInt(dodger.style.left) + 1 > 360) {
+		alert("edge of screen");
+		return;
+	} else {
+		dodger.style.left = parseInt(dodger.style.left) + 1 + "px";
+	}
+}
+
+document.addEventListener("keydown", function (event) {
+	switch (event.key) {
+		case "ArrowLeft":
+			moveDodgerLeft();
+			break;
+		case "ArrowRight":
+			moveDodgerRight();
+			break;
+		default:
+			alert("Press right or left arrow key to move the dodger");
+	}
+});
